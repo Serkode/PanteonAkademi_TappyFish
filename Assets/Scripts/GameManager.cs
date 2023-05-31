320bd0c1a010cfcdd33745b514c3dbf41f4e6ab3
Serkan <serkandede92@gmail.com>
2023-05-31 21:33:55 +0300

    Added PlayerPrefs and GameOver Panel
    Added PlayerPrefs and GameOver Panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static Vector2 bottomLeft;
     public static bool gameOver;
+    public GameObject gameOverPanel;
 
     private void Awake()
     {
         bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        gameOverPanel.SetActive(false);
     }
     void Start()
     {
@@ -19,6 +22,12 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         gameOver = true;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void restartBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     void Update()
